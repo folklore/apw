b4ca7226580bb1bbf66bfe35f295a06c1f428883
s <gsr@nkrf74.ru>
2014-05-08 10:52:55 +0600

    move sanitize_text from shit.rb to sheets_helper.rb

--- app/models/sheet.rb
+++ app/models/sheet.rb
@@ -59,45 +59,6 @@ class Sheet < ActiveRecord::Base
                              .first }
   end
 
-  # Делаем преобразование текста, не допуская
-  # в содержимое какие либо другие теги
-  def sanitize_text
-    Rails.cache.fetch("text_#{id}") {
-      Sanitize.clean(transformation,
-                     elements: %w(b i a),
-                     attributes: { 'a' => ['href'] } )
-    }
-  end
-
-  def transformation
-    # **string**      => <b>string</b>
-    # \\string\\      => <i>string</i>
-    # ((link anchor)) => <a href="[site]link">anchor</a>
-
-    b_regexp = /\*\*(.+)\*\*/m
-    i_regexp = /\\\\(.+)\\\\/m
-    a_regexp = /\(\(([a-z\/A-Z0-9_а-яёА-ЯЁ]+)\s(.+?)\)\)/
-
-    exit = false
-
-    # Собственно функция циклично будет обходить весь текст
-    # страницы пока не останется ни одного элемента, который
-    # нужно преобразовать согласно заданию
-    until exit do
-      if not (b = text.match(b_regexp)).nil?
-        text.sub!(b_regexp, "<b>#{b[1]}</b>")
-      elsif not (i = text.match(i_regexp)).nil?
-        text.sub!(i_regexp, "<i>#{i[1]}</i>")
-      elsif not (a = text.match(a_regexp)).nil?
-        text.sub!(a_regexp, "<a href='/#{a[1]}'>#{a[2]}</a>")
-      else
-        exit = true
-      end
-    end
-
-    return text
-  end
-
   after_save :drop_cache
 
   private
